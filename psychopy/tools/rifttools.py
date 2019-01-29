83c01c6c36b257bd666854f260fe6789ec51ba0b
mdcutone <cutonem@yorku.ca>
2019-01-29 17:15:56 -0500

    MISC: Test.

--- psychopy/tools/rifttools.py
+++ psychopy/tools/rifttools.py
@@ -10,20 +10,3 @@ Ontario, Canada
 # Part of the PsychoPy library
 # Copyright (C) 2018 Jonathan Peirce
 # Distributed under the terms of the GNU General Public License (GPL).
-
-import psychxr.ovr as ovr
-
-# math types exposed by PsychXR
-ovrSizei = ovr.math.ovrSizei
-ovrRect = ovr.math.ovrRecti
-ovrVector3f = ovr.math.ovrVector3f
-ovrMatrix4f = ovr.math.ovrMatrix4f
-ovrQuat = ovr.math.ovrQuatf
-ovrPosef = ovr.math.ovrPosef
-ovrFovPort = ovr.math.ovrFovPort
-
-# misc constants exposed by PsychXR
-OVR_EYE_LEFT = ovr.capi.ovrEye_Left
-OVR_EYE_RIGHT = ovr.capi.ovrEye_Right
-OVR_HAND_LEFT = 0
-OVR_HAND_RIGHT = 1
